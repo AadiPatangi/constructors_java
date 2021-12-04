9983940e50564e895148c692fbf8fd4843aa7494
Aadi Patangi <87159377+AadiPatangi@users.noreply.github.com>
2021-12-03 18:31:31 -0800

    Add files via upload

---
+++ book.java
@@ -0,0 +1,24 @@
+package mac_JavaProjects;
+
+public class book {
+
+	
+		public String title;
+		public String author;
+		public int pages;
+		public String language;
+		
+		public book(String title, String author, int pages, String language) {  //creating the class
+			this.title = title;
+			this.author = author;
+			this.pages = pages;
+			this.language = language;
+			
+			
+		}
+
+	}
+
+	
+
+
